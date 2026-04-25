992ef193033b369fa7851a52f3488db4b5fce77a
jandrews689 <89318319+jandrews689@users.noreply.github.com>
2026-04-25 08:46:20 +0100

    Update isaac_hardware_interface.py
    Added noise to the sim env

--- simulation/isaac/rl/interface/isaac_hardware_interface.py
+++ simulation/isaac/rl/interface/isaac_hardware_interface.py
@@ -5,6 +5,8 @@ from collections.abc import Sequence
 import torch
 from isaaclab.assets import Articulation
 from isaaclab.utils.math import quat_rotate_inverse
+from isaaclab.utils.noise import gaussian_noise
+from isaaclab.utils.noise import AdditiveGaussianNoiseCfg
 
 from .hardware_interface import BaseHardwareInterface, ControlPacket, ObservationPacket
 
@@ -17,6 +19,14 @@ class IsaacHardwareInterface(BaseHardwareInterface):
         self.joint_ids = joint_ids
         self.device = device
         self._gravity_vec_w = torch.tensor([0.0, 0.0, -1.0], dtype=torch.float32, device=device)
+
+        self._noise_cfg = {
+            "joint_pos": AdditiveGaussianNoiseCfg(std=0.002),
+            "joint_vel": AdditiveGaussianNoiseCfg(std=0.02),
+            "joint_effort": AdditiveGaussianNoiseCfg(std=0.5),
+            "gravity": AdditiveGaussianNoiseCfg(std=0.01),
+            "gyro": AdditiveGaussianNoiseCfg(std=0.01),
+        }
 
     def _resolve_env_ids(self, env_ids: Sequence[int] | None) -> torch.Tensor:
         if env_ids is None:
@@ -51,12 +61,26 @@ class IsaacHardwareInterface(BaseHardwareInterface):
             self._gravity_vec_w.unsqueeze(0).expand(len(env_ids_t), -1),
         )
 
+        joint_pos = self.robot.data.joint_pos[env_ids_t].clone()
+        joint_vel = self.robot.data.joint_vel[env_ids_t].clone()
+        joint_effort = self._get_joint_effort_obs()[env_ids_t].clone()
+        projected_gravity_b = projected_gravity_b.clone()
+        imu_gyro_b = self.robot.data.root_ang_vel_b[env_ids_t].clone()
+
+        joint_pos = gaussian_noise(joint_pos, self._noise_cfg["joint_pos"])
+        joint_vel = gaussian_noise(joint_vel, self._noise_cfg["joint_vel"])
+        joint_effort = gaussian_noise(joint_effort, self._noise_cfg["joint_effort"])
+        projected_gravity_b = gaussian_noise(projected_gravity_b, self._noise_cfg["gravity"])
+        imu_gyro_b = gaussian_noise(imu_gyro_b, self._noise_cfg["gyro"])
+
+        projected_gravity_b = projected_gravity_b / torch.norm(projected_gravity_b, dim=1, keepdim=True)
+
         return ObservationPacket(
-            joint_pos=self.robot.data.joint_pos[env_ids_t].clone(),
-            joint_vel=self.robot.data.joint_vel[env_ids_t].clone(),
-            joint_effort=self._get_joint_effort_obs()[env_ids_t].clone(),
-            projected_gravity_b=projected_gravity_b.clone(),
-            imu_gyro_b=self.robot.data.root_ang_vel_b[env_ids_t].clone(),
+            joint_pos = joint_pos,
+            joint_vel = joint_vel,
+            joint_effort = joint_effort,
+            projected_gravity_b = projected_gravity_b,
+            imu_gyro_b = imu_gyro_b,
         )
 
     def write_control_packet(self, packet: ControlPacket, env_ids: Sequence[int] | None = None) -> None:
